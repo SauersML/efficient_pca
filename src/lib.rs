6578737b5199302b8b46a3b19372a387d927e087
SauersML <123714540+SauersML@users.noreply.github.com>
2025-05-23 16:54:53 -0500

    Create lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+#![doc = include_str!("../README.md")] // Crate-level documentation
+
+pub mod pca;
+pub mod eigensnp;
+
+
+pub use pca::PCA;
+
+// Re-export key items from the eigensnp module for users of the EigenSNP functionality.
+pub use eigensnp::{
+    EigenSNPCoreAlgorithm,
+    EigenSNPCoreAlgorithmConfig,
+    EigenSNPCoreOutput,
+    LdBlockSpecification,
+    PcaReadyGenotypeAccessor,
+    PcaSnpId,
+    QcSampleId,
+    LdBlockListId,
+    CondensedFeatureId,
+    PrincipalComponentId,
+};
+
+#[cfg(test)]
+mod tests;
